f2ec729fd7d9cb5e2575f2c89a2d6503c42974d1
Parth Solanki <parthsolanki1498@gmail.com>
2024-12-31 21:07:55 -0500

    last commit of 2024 eh

--- src/App.js
+++ src/App.js
@@ -1,43 +1,28 @@
-import logo from './logo.svg';
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import TextBlock from './textBlock';
 import './App.css';
-import Home from './Home';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <Parallax pages={2} style={{ top: '0', left: '0' }} className="animation">
-          {/* Background Layer */}
-          <ParallaxLayer offset={0} speed={0.25}>
-            <div className="animation_layer parallax" id="artback"></div>
-          </ParallaxLayer>
+      <Parallax pages={2} style={{ top: "0", left: "0" }} class="animation">
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div className="animation_layer parallax" id="artback">
 
-          {/* Foreground Layer */}
-          <ParallaxLayer offset={0} speed={0.5}>
-            <div className="animation_layer parallax" id="man1"></div>
-          </ParallaxLayer>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.7}>
+          <div className="animation_layer parallax" id="whitepalacegreenary">
 
-          {/* Sticky Text Block */}
-          <ParallaxLayer
-            sticky={{ start: 0, end: 1 }} // Stick from page 0 to 1
-            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-          >
-            <div id="textblock">
-              <div id="textblock-container">
-                <div id="textblock-title">Welcome to Gita</div>
-              </div>
-            </div>
-          </ParallaxLayer>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div className="animation_layer parallax" id="whitepalace">
 
-          {/* Home Page */}
-          <ParallaxLayer offset={2} speed={0.5}>
-            <div className="home">
-              <Home />
-            </div>
-          </ParallaxLayer>
-        </Parallax>
-      </header>
+          </div>
+        </ParallaxLayer>
+        
+      </Parallax>
     </div>
   );
 }
