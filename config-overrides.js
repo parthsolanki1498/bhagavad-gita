a9cbc1276744b99c5171cb36b4afdc0d14ed04e1
Parth Solanki <parthsolanki1498@gmail.com>
2025-01-11 22:39:25 -0500

    overiding config

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,19 @@
 const path = require('path');
 
-module.exports = {
-  webpack: (config, env) => {
-    config.resolve.fallback = {
-      url: require.resolve('url/'),
-      // Other Node.js polyfills can be added here if needed
-    };
-    return config;
-  }
+// module.exports = {
+//   webpack: (config, env) => {
+//     config.resolve.fallback = {
+//       url: require.resolve('url/'),
+//       // Other Node.js polyfills can be added here if needed
+//     };
+//     return config;
+//   }
+// };
+
+module.exports = function override(config, env) {
+  console.log("React app rewired works!")
+  config.resolve.fallback = {
+    fs: false
+  };
+  return config;
 };
